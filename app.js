bc94e7aeb4632f205c53f25c09ec2ceb979c826f
codebyred <nazmulhaqueredoan@gmail.com>
2024-01-29 22:48:30 +0600

    feat:add api registry file and api registry endpoint

--- app.js
+++ app.js
@@ -1,47 +1,82 @@
 import express from "express";
-import httpProxy from "http-proxy"
 import cors from "cors"
+import {readFile, writeFile} from "fs/promises";
+import { write } from "fs";
 
 const app = express();
-
-const proxy = httpProxy.createProxyServer();
 
 app.use(cors(
     {
         origin:[
             "http://localhost:5173",
-            "http://localhost:5174"
+            "http://localhost:5174",
+            "http://localhost:3008",
         ]
     }
 ));
 
-app.use((req, res, next)=>{
+app.use(express.json());
 
-    if(req.url.startsWith("/api/login"))
-        return proxy.web(req, res, {target:"http://localhost:3006"});
+app.post("/register", async(req, res)=>{
+    const registryInfo = req.body;
+    try{
+        const serviceRegistryFile = await readFile("./serviceRegistry.json","utf8");
+        const serviceRegistry = JSON.parse(serviceRegistryFile);
+        const services = serviceRegistry.services;  
 
-    if(req.url.startsWith("/api/worker"))
-        return proxy.web(req, res, {target:"http://localhost:3007"});
+        services[registryInfo.api] = {...registryInfo};
+        await writeFile("./serviceRegistry.json", JSON.stringify(serviceRegistry));
+        res.json({msg:"registered"});
 
-    if(req.url.startsWith("/api/user"))
-        return proxy.web(req, res, {target:"http://localhost:3008"});
-
-    return next();
+    }catch(e){
+        res.status(500).json({msg:"internal server error"});
+    }
 });
 
-app.use("api/user", (req, res)=>{
-    proxy.web(req, res, {target:""});
-})
+app.all("/:api/:path", async(req, res)=>{
 
+    const api = req.params.api;
+    const path = req.params.path;
 
-proxy.on('error', (err, req, res) => {
-    console.error('Proxy error:', err);
-    res.status(500).send('Proxy error');
+    try{
+        const serviceRegistryFile = await readFile("./serviceRegistry.json","utf8");
+        const serviceRegistry = JSON.parse(serviceRegistryFile);
+        const services = serviceRegistry.services;  
+
+        if(!services[api]){
+            return res.send("api does not exist");
+        }
+
+        const apiUrl = services[api].url + "/" + api + "/" + path;
+
+        const fetchOptions = {
+            method: req.method,
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        }
+
+        if(req.body){
+            fetchOptions.data = JSON.stringify(req.body);
+        }
+
+        const  apiResponse = await fetch(apiUrl, fetchOptions);
+
+        const apiData = await apiResponse.json();
+    
+        return res.json(apiData);
+
+    }catch(e){
+
+        return res.status(500).send("internal server error");
+
+    }
 });
+
 
 
 app.listen(3000,()=>{
     console.log("gateway server started")
 });
-
-
