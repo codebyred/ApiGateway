98096f3ef512d21befb538100fa7e74c218890e1
Redoan <nazmulhaqueredoan@gmail.com>
2024-05-06 23:32:06 +0600

    changes for testing

--- app.js
+++ app.js
@@ -82,6 +82,67 @@ app.post("/api/booking/fail", async (req, res)=>{
     
 })
 
+app.all("/api/:service/:endpoint1/:endpoint2/:param", async(req, res)=>{
+
+    const service = req.params.service;
+    const endpoint1 = req.params["endpoint1"];
+    const endpoint2 = req.params["endpoint2"];
+    const param = req.params["param"]
+
+    try{
+        const serviceRegistryFile = await readFile("./serviceRegistry.json","utf8");
+        const serviceRegistry = JSON.parse(serviceRegistryFile);
+        const services = serviceRegistry.services;  
+
+        if(!services[service])
+            throw new Error("service does not exist");
+        
+
+        let serviceEndpoint;
+
+        if(!endpoint1 && !endpoint2)
+            serviceEndpoint = `${services[service].url}/api/${services[service].service}`;
+
+        if(endpoint1) 
+            serviceEndpoint =  `${services[service].url}/api/${services[service].service}/${endpoint1}`;
+        
+        if(endpoint2)
+            serviceEndpoint = `${serviceEndpoint}/${endpoint2}`
+        
+        if(param)
+            serviceEndpoint = `${serviceEndpoint}/${param}`
+
+        if(req.query?.clientId){
+            serviceEndpoint = `${serviceEndpoint}?clientId=${req.query?.clientId}`
+        }
+            
+        
+        const fetchOptions = {
+            method: req.method,
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+        }
+
+        if(Object.keys(req.body).length > 0){
+            fetchOptions.body = JSON.stringify(req.body);
+        }
+        console.log(serviceEndpoint)
+        const  serviceResponse = await fetch(serviceEndpoint, fetchOptions);
+        const serviceData = await serviceResponse.json();   
+
+        console.log(serviceResponse)
+    
+        return res.status(200).json(serviceData);
+
+    }catch(e){
+
+        return res.status(500).json({msg:"gateway server error"});
+
+    }
+});
 
 app.all("/api/:service/:endpoint?", async(req, res)=>{
 
@@ -108,8 +169,6 @@ app.all("/api/:service/:endpoint?", async(req, res)=>{
             serviceEndpoint = `${serviceEndpoint}?clientId=${req.query?.clientId}`
         }
             
-    
-
         const fetchOptions = {
             method: req.method,
             mode: "cors",
@@ -124,9 +183,7 @@ app.all("/api/:service/:endpoint?", async(req, res)=>{
         }
 
         const  serviceResponse = await fetch(serviceEndpoint, fetchOptions);
-        const serviceData = await serviceResponse.json();
-
-        
+        const serviceData = await serviceResponse.json();   
     
         return res.status(200).json(serviceData);
 
